0f562bca20ef2faac31084ecec1b3c8cb1645740
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-11-10 13:11:22 +0300

    Initial commit

---
+++ src/main/java/com/aplavina/CoordinateDescentMinimizer.java
@@ -0,0 +1,8 @@
+package com.aplavina;
+
+import java.util.function.BiFunction;
+
+public class CoordinateDescent {
+    private final BiFunction<Double, Double, Double> function;
+
+}
---
+++ src/main/java/com/aplavina/Newton.java
@@ -0,0 +1,4 @@
+package com.aplavina;
+
+public class Newton {
+}
